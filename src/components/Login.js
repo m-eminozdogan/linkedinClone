b572d060fda4f80bc14922ce23cad2547e0782a2
M Emin Özdoğan <m.eminozdogan@hotmail.com>
2022-01-10 23:04:07 +0300

    login page's section done

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,18 @@ const Login = (props) => {
                     <SignIn>Sign in</SignIn>
                 </div>
             </Nav>
-
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img alt='#' src='/images/login-hero.svg' />
+                </Hero>
+                <Form>
+                    <Google>
+                        <img alt='#' src='/images/google.svg' />
+                        Sign in with google
+                    </Google>
+                </Form>
+            </Section>
         </Container>
     )
 }
@@ -49,13 +60,13 @@ const Join = styled.a`
     }
 `
 const SignIn = styled.a`
-    box-shadow:inset 0 0 0 1px #0a66c2;
+     box-shadow:inset 0 0 0 1px #0a66c2;
     color:#0a66c2;
     border-radius:24px;
     transition-duration:167ms;
     font-size:16px;
     font-weight:600;
-    line-height:40;
+    line-height:40px;
     padding:10px 24px;
     text-align:center;
     background-color:rgba(0,0,0,0);
@@ -63,6 +74,84 @@ const SignIn = styled.a`
         background-color:rgba(112,181,249,0.15);
         text-decoration:none;
         color:#0a66c2;
+    } 
+`
+const Section = styled.section`
+    display:flex;
+    align-content:flex-start;
+    min-height:700px;
+    padding-bottom:138px;
+    padding-top:40px;
+    padding:60px 0;
+    position:relative;
+    flex-wrap:wrap;
+    width:100%;
+    max-width:1128px;
+    align-items:center;
+    margin:auto;
+    @media(max-width:768px){
+        margin:auto;
+        min-height:0px;
+    }
+`
+const Hero = styled.div`
+    width:100%;
+    h1{
+        padding-bottom:0;
+        width:55%;
+        font-size:56px;
+        color:#2977c9;
+        font-weight:200;
+        line-height:70px;
+        @media(max-width:768px){
+            text-align: center;
+            font-size:20px;
+            width: 100%;
+            line-height:2;
+        }
+    }
+    img{
+        /* z-index:-1; */
+        width:700px;
+        height: 670px;
+        position: absolute;
+        bottom:-2px;
+        right:-150px;
+        @media(max-width:768px){
+            top:230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
+`
+const Form = styled.form`
+    margin-top:100px;
+    width:408px;
+    @media(max-width:768px){
+        margin-top:20px;
+
+    }
+`
+const Google = styled.button`
+    display:flex;
+    justify-content:center;
+    background-color:#fff;
+    align-items:center;
+    height:56px;
+    width:100%;
+    border-radius:28px;
+    box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), inset 0 0 0 2px rgb(0 0 0 / 0%);
+    /* inset 0 0 0 1px rgb(0 0 0 / 0%);  */
+    vertical-align:middle;
+    z-index:0;
+    transition-duration:167ms;
+    font-size:20px;
+    color:rgba(0,0,0,0.6);
+    &:hover{
+        background-color:rgba(207,207,207,0.25);
+        color:rgba(0,0,0,0.75);
+
     }
 `
 export default Login
