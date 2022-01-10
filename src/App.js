b7209ef63884a71f5522a05c32c1b7af910f4389
M Emin Özdoğan <m.eminozdogan@hotmail.com>
2022-01-10 21:11:00 +0300

    login page's navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Start the building
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
